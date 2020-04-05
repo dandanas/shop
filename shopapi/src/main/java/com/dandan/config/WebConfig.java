7f7fd5384fa84ca0269e302ac9169baf9772cc4a
dandan <15272027675@163.com>
2020-04-05 13:55:14 +0800

    跨域问题

--- shopapi/src/main/java/com/dandan/config/WebConfig.java
+++ shopapi/src/main/java/com/dandan/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.dandan.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,6 +16,13 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(tokenInterceptor).addPathPatterns("/**")
 		.excludePathPatterns("/user/login")
-		.excludePathPatterns("/user/register");
+		.excludePathPatterns("/user/register")
+		.excludePathPatterns("/welcome");
+	}
+
+	//跨域问题
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/*").allowedOrigins("*");
 	}
 }
